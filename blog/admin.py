0821069524c0906a14339d0e7c7780272c9fc878
Farzad Nari <farzadnari10@gmail.com>
2025-05-03 18:38:43 +0330

    register admin for ArticleImage and ArticleRequest models

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Article)
 admin.site.register(ArticleCategory)
+admin.site.register(Article)
+admin.site.register(ArticleImage)
+admin.site.register(ArticleRequest)
+
 
