043e78e70f579f93b618f34d3d5fc4d26e3181be
Farzad Nari <farzadnari10@gmail.com>
2025-05-04 01:05:24 +0330

    define update_title_and_slug_on_delete signal

--- blog/signals.py
+++ blog/signals.py
@@ -1,7 +1,7 @@
 from utils import update_descendants_active_status
 from .models import ArticleCategory, Article
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.contrib.postgres.search import SearchVector
 
 
@@ -17,4 +17,13 @@ def update_search_vector(sender, instance, **kwargs):
     ).update(
         sv=SearchVector('title')
     )
+
+
+@receiver(pre_save, sender=Article)
+def update_title_and_slug_on_delete(sender, instance, **kwargs):
+    if instance.pk:
+        original_article = Article.objects.get(pk=instance.pk)
+        if original_article.is_deleted != instance.is_deleted and instance.is_deleted:
+            instance.title = f"{instance.title} del"
+            instance.slug = f"{instance.slug}-del"
 
