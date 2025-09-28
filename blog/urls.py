fa852b6a87a5919d4d116922e1ace02dae04ffed
Farzad Nari <farzadnari10@gmail.com>
2025-09-28 08:53:31 +0330

    add categories/ and categories/select/ urls in blog.urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 
 urlpatterns = [
+
+    
+    path('categories/', views.CategoryListView.as_view(),name='category-list'),
+    path('categories/select/', views.CategorySelectView.as_view(),name='category-select'),
+
     path('public/articles/', views.PublicArticleViewSet.as_view({'get': 'list'}), name='public-articles-list'),
     path('public/articles/<slug:slug>/', views.PublicArticleViewSet.as_view({'get': 'retrieve'}), name='public-articles-detail'),
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
