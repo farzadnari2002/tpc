6298ca239e1663cb65c60d1c0bf807482cb0461d
Farzad Nari <farzadnari10@gmail.com>
2025-09-28 08:54:17 +0330

    remove old categories url from blog.urls

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
 
     path('public/articles/', views.PublicArticleViewSet.as_view({'get': 'list'}), name='public-articles-list'),
     path('public/articles/<slug:slug>/', views.PublicArticleViewSet.as_view({'get': 'retrieve'}), name='public-articles-detail'),
-    path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('author/articles/', views.AuthorArticleViewset.as_view({'get':'list'}), name='author-articles-list')
 ]
