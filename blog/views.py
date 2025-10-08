337e505e0f61a7dc668ad7676e6e6a3cf33a2719
Farzad Nari <farzadnari10@gmail.com>
2025-10-08 18:29:36 +0330

    add cancel_request, partial_update and destroy methods  in ArticleRequestViewSet

--- blog/views.py
+++ blog/views.py
@@ -62,4 +62,44 @@ class ArticleRequestViewSet(viewsets.ViewSet):
             return Response([_("درخواست ارسال شد.")], status=status.HTTP_200_OK)
         return Response([_("امکان ارسال درخواست برای این وضعیت نیست.")], status=status.HTTP_400_BAD_REQUEST)
 
-#
+    def cancel_request(self, request, pk=None):
+        queryset = get_object_or_404(ArticleRequest, author=request.user, pk=pk, is_deleted=False)
+        
+        if queryset.status == RequestStatusChoices.PENDING:
+            if queryset.need_revision:
+                queryset.status = RequestStatusChoices.NEED_REVISION
+            else:
+                queryset.status = RequestStatusChoices.DRAFT
+            queryset.save()
+            return Response([_('درخواست لغو شد.')], status=status.HTTP_200_OK)
+        return Response([_("امکان لغو درخواست برای این وضعیت نیست.")], status=status.HTTP_400_BAD_REQUEST)  
+    
+    def partial_update(self, request, pk=None):
+        queryset = get_object_or_404(ArticleRequest, author=request.user, pk=pk, is_deleted=False)
+        serializer = self.serializer_class(queryset, data=request.data, partial=True)
+        
+        check_status = bool(queryset.status in [RequestStatusChoices.DRAFT, RequestStatusChoices.NEED_REVISION])
+        if not check_status:
+            error_message = {"error": _("در این وضعیت امکان ویرایش درخواست وجود ندارد.")}
+            return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+        
+        if serializer.is_valid():
+            try:
+                queryset.clean()
+                serializer.save()
+            except Exception as e:
+                raise serializers.ValidationError(str(e))
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        queryset = get_object_or_404(ArticleRequest, author=request.user, pk=pk, is_deleted=False)
+        if queryset.status == RequestStatusChoices.DRAFT:
+            queryset.is_deleted = True
+            try:
+                queryset.save()
+            except Exception as e:
+                raise serializers.ValidationError(str(e))
+            return Response([_("با موفقیت حذف شد.")], status=status.HTTP_204_NO_CONTENT)
+        return Response([_("امکان حذف ممکن نیست")], status=status.HTTP_400_BAD_REQUEST)
