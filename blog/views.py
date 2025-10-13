98af7678210b32aea75e11bfb0b1c7ef22e8d0ea
Farzad Nari <farzadnari10@gmail.com>
2025-10-13 15:01:59 +0330

    add AuthorUploadImageViewSet in blog.views

--- blog/views.py
+++ blog/views.py
@@ -30,6 +30,20 @@ class AuthorArticleViewset(ReadOnlyModelViewSet):
         return Article.objects.filter(author=self.request.user, status=Article.STATUS.PUBLISHED)
 
 
+class AuthorUploadImageViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = None
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request':request})
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+
+
 class ArticleRequestViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = None
